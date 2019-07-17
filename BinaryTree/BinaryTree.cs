b6e6c813a5023c4c10a8f45108d20732bf6054d2
Vincent Mackay <mackv42@gmail.com>
2019-07-17 13:00:48 -0500

    init

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+
+    public class Node
+    {
+        public int item;
+        public Node Left;
+        public Node Right;
+
+        public Node(int item)
+        {
+            Left = null;
+            Right = null;
+            this.item = item;
+        }
+    }
+
+    public class BinaryTree
+    {
+        Node head;
+
+        public BinaryTree()
+        {
+            head = null;
+        }
+
+
+        public Node FindNode( int n )
+        {
+            Node tmp = head;
+
+            while (tmp != null)
+            {
+                if (tmp.item == n)
+                {
+                    return tmp;
+                }
+
+                tmp = (n < tmp.item) ? tmp.Left : tmp.Right;
+            }
+
+            return null;
+        }
+
+        public void AddNode( int n )
+        {
+            if (head == null)
+            {
+                head = new Node(n);
+                return;
+            }
+            Node tmp = head;
+
+            while ( true ) {
+                if (n < tmp.item)
+                {
+                    if(tmp.Left == null)
+                    {
+                        tmp.Left = new Node(n);
+                        break;
+                    }
+
+                    tmp = tmp.Left;
+                }
+                else
+                {
+                    if (tmp.Right == null)
+                    {
+                        tmp.Right = new Node(n);
+                        break;
+                    }
+
+                    tmp = tmp.Right;
+                }
+            }
+        }
+    }
+}
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree b = new BinaryTree();
+            b.AddNode(100);
+
+            //left subtree
+            b.AddNode(75);
+            b.AddNode(83);
+            b.AddNode(64);
+
+            //Right subtree
+            b.AddNode(150);
+            b.AddNode(167);
+            b.AddNode(125);
+
+            Console.WriteLine(b.FindNode(125).item);
+        }
+    }
+}
